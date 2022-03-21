93638f59471112151a1b208516953bcfc5c3d702
Larissa.rocha <larissa.rocha@econocom.com.br>
2022-03-21 11:21:45 -0300

    projeto aprendizado JavaScript Alura

---
+++ js/calcula-imc.js
@@ -0,0 +1,66 @@
+
+
+var titulo = document.querySelector(".titulo");
+
+titulo.textContent = "Aparecida Nutricionista";
+
+
+var pacientes = document.querySelectorAll(".paciente");
+
+
+for (var i = 0 ; i < pacientes.length; i++) {
+	var paciente = pacientes[i];
+
+
+	var tdPeso = paciente.querySelector(".info-peso");
+	var peso = tdPeso.textContent;
+
+	var tdAltura = paciente.querySelector(".info-altura");
+	var altura = tdAltura.textContent;
+
+	var tdImc = paciente.querySelector(".info-imc");
+
+	var alturaValida = validaAltura(altura);
+	var pesoValido = validaPeso(peso);
+
+	if(!pesoValido){
+		pesoValido = false;
+		tdImc.textContent = "Peso invalido!";
+		paciente.classList.add("paciente-invalido");
+	}
+
+	if(!alturaValida){
+		alturaValida = false;
+		tdImc.textContent = "Altura invalida!";
+		paciente.style.color ="red"
+		paciente.classList.add("paciente-invalido");
+	}
+
+	if (alturaValida && pesoValido){
+		var imc = calculaImc(peso,altura);
+		tdImc.textContent = imc;
+	}
+}
+
+function calculaImc(peso,altura) {
+	var imc = 0;
+
+	imc = peso / (altura * altura);
+
+	return imc.toFixed(1);
+}
+function validaPeso(peso){
+	if( peso >= 0 && peso < 200){
+		return true;
+	} else{
+		return false;
+	}
+	
+}
+function validaAltura(altura){
+	if(altura >= 0 && altura < 3){
+		return true;
+	}else{
+		return false;
+	}
+}
